0fa63f16dc1ad4558bb8ec69ca0900650e366257
mohammadadnansaddayat-arch <mohammadadnansaddayat@bafsemc.edu.bd>
2025-09-23 19:13:41 +0600

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the first friend number of apple: "))
+b =int(input("Enter the second friend number of apple: "))
+c = int(input("Enter the third friend number of apple: "))
+d = int(input("Enter the fourth friend number of apple: "))
+e = int(input("Enter the fifth friend number of apple: "))
+
+total = a + b + c + d + e
+
+avg = total / 5
+
+print("every friend will have: ", avg)
