5ae81359c2abc1675936ebd725cb65f20a410036
mohammadadnansaddayat-arch <mohammadadnansaddayat@bafsemc.edu.bd>
2025-09-26 13:18:09 +0600

    Add files via upload

---
+++ H.W. avg.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the first friend number of orange: "))
+b =int(input("Enter the second friend number of orange: "))
+c = int(input("Enter the third friend number of orange: "))
+d = int(input("Enter the fourth friend number of orange: "))
+
+total = a + b + c + d 
+
+
+avg = total / 4
+
+print("Every friend will have: ", avg)
