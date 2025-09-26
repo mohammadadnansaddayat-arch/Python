b2982db7de4e0b5e25da9ff477f51ceaa5a6f423
mohammadadnansaddayat-arch <mohammadadnansaddayat@bafsemc.edu.bd>
2025-09-26 19:03:11 +0600

    Add files via upload

---
+++ area.py
@@ -0,0 +1,6 @@
+base = float(input("Enter the base of tringle: "))
+height = float(input("Enter the height of tringle: "))
+
+area = (base*height) / 2
+
+print("The area of the triangle is: ", area)
