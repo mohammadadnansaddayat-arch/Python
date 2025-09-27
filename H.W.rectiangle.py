b8215923146aeb2d1b5ef7a54d8d4e848fee100c
mohammadadnansaddayat-arch <mohammadadnansaddayat@bafsemc.edu.bd>
2025-09-27 20:05:11 +0600

    Add files via upload

---
+++ H.W.rectiangle.py
@@ -0,0 +1,6 @@
+length = float(input("Enter the length of rectringle: "))
+height = float(input("Enter the height of rectringle: "))
+
+area = (length*height) / 2
+
+print("The area of the rectriangle is: ", area)
