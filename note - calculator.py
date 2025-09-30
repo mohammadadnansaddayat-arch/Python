a13096a626c3e00236e8ff20ce1dcd69a21c8702
mohammadadnansaddayat-arch <mohammadadnansaddayat@bafsemc.edu.bd>
2025-09-30 19:18:04 +0600

    Add files via upload

---
+++ note - calculator.py
@@ -0,0 +1,21 @@
+n = int(input("Enter the number of notes: "))
+
+note = n // 500
+print("500tk: ",note)
+n = n % 500
+
+note = n // 100
+print("100k: ",note)
+n = n % 100
+
+note = n // 50
+print("50tk: ",note)
+n = n % 50
+
+note = n // 20
+print("20tk: ",note)
+n = n % 20
+
+note = n // 10
+print("10tk: ",note)
+n = n % 10
