b39a05a0a3170bdeaa43328c019e568d49e96ddf
mohammadadnansaddayat-arch <mohammadadnansaddayat@bafsemc.edu.bd>
2025-10-03 19:27:25 +0600

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+if a > b:
+    print("The first number is greater tha the second number.")
+else:
+    print("The second number is greater than the first number.")
---
+++ smaller.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+if a < b:
+    print("The first number is smaller than the second number.")
+else:
+    print("The second number is smaller than the first number.")
+    
