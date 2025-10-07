09a943cb01938fdef7b39f6a90e775000ac29ed6
mohammadadnansaddayat-arch <mohammadadnansaddayat@bafsemc.edu.bd>
2025-10-07 19:28:39 +0600

    Add files via upload

---
+++ log in if you can.py
@@ -0,0 +1,11 @@
+print("login if you can.")
+username = input("Enter your username: ")
+
+if username == "Masaayat7":
+    password = input("Enter your password: ")
+    if password == "@**890":
+        print("Login susccessfully!")
+    else:
+        print("Incorrect password!Try again.")
+else:
+    print("User not found")
