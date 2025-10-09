bd690fd7689ee38b6cb2ccca6f7fc6c1eb5f3ee2
mohammadadnansaddayat-arch <mohammadadnansaddayat@bafsemc.edu.bd>
2025-10-09 18:34:33 +0600

    Add files via upload

---
+++ H.W.log in if you can.py
@@ -0,0 +1,9 @@
+print("login.")
+username = input("Enter your username: ")
+
+if username == "@ahsan34":
+    grade = input("Enter your grade: ")
+    if grade == "3":
+        print("Login susccessfully!")
+    else:
+        print("login failed.")
