ceae6cb7317baa6d3021fee531ff1b250a7d407f
AYAT <mohammadadnansaddayat@bafsemc.edu.bd>
2025-10-17 16:17:35 +0600

    Add files via upload

---
+++ H.W.login.py
@@ -0,0 +1,11 @@
+print("login if you can.")
+username = input("Enter your username: ")
+
+if username == "Masaayat8":
+    password = input("Enter your password: ")
+    if password == "1-7-@-A-Y-A-T":
+        print("Login susccessfully!")
+    else:
+        print("Incorrect password!Try again.")
+else:
+    print("User not found")
