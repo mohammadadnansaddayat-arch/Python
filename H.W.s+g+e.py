9e879189316c0edf888db8ea5895e4e7215299ad
AYAT <mohammadadnansaddayat@bafsemc.edu.bd>
2025-10-19 20:28:34 +0600

    Add files via upload

---
+++ H.W.s+g+e.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+if a != b:
+    print("The first number is smaller than the second number.")
+else:
+    print("The second number is smaller than the first number.")
