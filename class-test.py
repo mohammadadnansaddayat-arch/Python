768a3bb2a350ad2ce0e745b8428cfdab7decbcf5
AYAT <mohammadadnansaddayat@bafsemc.edu.bd>
2025-11-12 19:40:56 +0600

    Add files via upload

---
+++ class-test.py
@@ -0,0 +1,9 @@
+print("Log in")
+
+username = str(input("Enter your username: "))
+if username == "MASAAYAT8":
+    grade = str(input("Enter your grade: "))
+    if grade == "2":
+        print("Log in suscesfully!")
+    else:
+        print("Log in failed.Try again...")
