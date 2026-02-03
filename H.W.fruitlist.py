fe68e7ef38c02f9060ca01b49b50100e6174c869
AYAT <mohammadadnansaddayat@bafsemc.edu.bd>
2026-02-03 14:01:46 +0600

    Add files via upload

---
+++ H.W.fruitlist.py
@@ -0,0 +1,15 @@
+fruits = ["mango","banna","strawberry"]
+
+print("fruits list:", fruits)
+
+fruits.append("watermelon")
+print("After added a fruit:", fruits)
+
+fruits.extend(["guava", "blueberry", "rassberry", "blackberry", "orange"])
+print("After added some furits:", fruits)
+
+fruits.sort()
+print("After sorted the fruit:", fruits)
+
+fruits.reverse()
+print("After reversed the fruits:", fruits)
